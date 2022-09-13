a8efb46fc96da81e5af28bf716e065f667f0546f
魏振杰 <Kogler_S@outlook.com>
2022-09-13 19:43:12 +0800

    22w04b
    添加StatusBar，修复在子线程修改主窗口标题时的卡死问题

--- visual_plat/canvas_deputy/state_deputy.py
+++ visual_plat/canvas_deputy/state_deputy.py
@@ -4,11 +4,10 @@ import time
 from enum import Enum
 from dataclasses import dataclass
 
-from PySide6.QtCore import QMutex
-
 from visual_plat.render_layer.layer_base import LayerBase
 from visual_plat.global_proxy.config_proxy import ConfigProxy
 from visual_plat.global_proxy.async_proxy import AsyncProxy
+from visual_plat.shared.utility.status_bar import StatusBar
 
 
 class RecordType(Enum):
@@ -32,8 +31,10 @@ class Record:
 class StateDeputy:
     record_path = ""
 
-    def __init__(self, layers: dict[str, LayerBase]):
+    def __init__(self, layers: dict[str, LayerBase], status_bar: StatusBar):
+        super().__init__()
         self.layers = layers
+        self.status_bar = status_bar
         self.record: Record = Record([], [])
         self.pausing = False  # 播放暂停
         self.blocked = False  # 异步线程阻塞
@@ -43,7 +44,6 @@ class StateDeputy:
         self.play_index = 0
         self.play_range = None
         self.play_record = None
-        self.play_mutex = QMutex()
         StateDeputy.record_path = ConfigProxy.path("record")
         if not os.path.exists(StateDeputy.record_path):
             os.mkdir(StateDeputy.record_path)
@@ -51,24 +51,30 @@ class StateDeputy:
     def block(self):
         """切换阻塞状态"""
         self.blocked = not self.blocked
+        if self.blocked:
+            self.status_bar.set("Blocked")
+        else:
+            self.status_bar.reset("Blocked")
 
     def reload(self, layer_tag: str, data=None, new_step=True):
         """重载某个图层"""
-        self.layers[layer_tag].reload(data)
-        record = RecordUnit(layer_tag, RecordType.reload, data)
-        if self.recording:
-            if new_step:
-                self.record.updates.append([])
-            self.record.updates[-1].append(record)
+        if layer_tag in self.layers.keys():
+            self.layers[layer_tag].reload(data)
+            record = RecordUnit(layer_tag, RecordType.reload, data)
+            if self.recording:
+                if new_step:
+                    self.record.updates.append([])
+                self.record.updates[-1].append(record)
 
     def adjust(self, layer_tag: str, data=None, new_step=True):
         """调整某个图层"""
-        self.layers[layer_tag].adjust(data)
-        record = RecordUnit(layer_tag, RecordType.adjust, data)
-        if self.recording:
-            if new_step:
-                self.record.updates.append([])
-            self.record.updates[-1].append(record)
+        if layer_tag in self.layers.keys():
+            self.layers[layer_tag].adjust(data)
+            record = RecordUnit(layer_tag, RecordType.adjust, data)
+            if self.recording:
+                if new_step:
+                    self.record.updates.append([])
+                self.record.updates[-1].append(record)
 
     @staticmethod
     def load_record(path):
@@ -100,15 +106,15 @@ class StateDeputy:
     def start_record(self):
         """开始录制"""
         if not self.recording:
-            print("Recording")
+            self.status_bar.set("Recording")
             self.record = Record([], [])
             for tag, layer in self.layers.items():
                 self.record.initial.append(RecordUnit(tag, RecordType.reload, layer.data))
             self.recording = True
+            self.status_bar.set("Recording")
 
     def start_replay(self, record: Record):
         """开始播放"""
-        print("start")
         initial = record.initial
         for r in initial:
             self.reload(r.layer_tag, r.record_data)
@@ -119,23 +125,21 @@ class StateDeputy:
             # 避免在刷新snapshot时设置为悬置状态
             self.replaying = True
             self.suspended = True  # 不再接受外部更新
+            self.status_bar.set("Replaying")
+            self.status_bar.set("Suspended")
             AsyncProxy.run(self.async_replay)
 
     def replay_by_index(self):
         """播放某一帧"""
-        self.play_mutex.lock()
         updates = self.play_record.updates
         if self.play_index in self.play_range:
-            print(f"playing {self.play_index}/{len(updates) - 1}")
+            self.status_bar.set("Replaying", f"{self.play_index + 1}/{len(updates)}")
             for upd in updates[self.play_index]:
                 if upd.record_type == RecordType.reload:
                     self.reload(upd.layer_tag, upd.record_data)
                 elif upd.record_type == RecordType.adjust:
                     self.adjust(upd.layer_tag, upd.record_data)
                     print("Warning: Adjustment not yet well supported.")
-                else:
-                    raise
-        self.play_mutex.unlock()
 
     def async_replay(self):
         """异步播放控制"""
@@ -166,17 +170,28 @@ class StateDeputy:
         """暂停"""
         if self.replaying:
             self.pausing = not self.pausing
+            if self.pausing:
+                self.status_bar.set("Paused")
+            else:
+                self.status_bar.reset("Paused")
         else:
             self.suspended = not self.suspended
+            if self.suspended:
+                self.status_bar.set("Suspended")
+            else:
+                self.status_bar.reset("Suspended")
 
     def terminate(self):
         """终止（优先终止播放，再次触发会终止录制）"""
         if self.replaying:
             self.replaying = False
             self.suspended = False
+            self.status_bar.reset("Replaying")
+            self.status_bar.reset("Suspended")
             self.play_index = self.play_range[-1]
             print("Replaying terminated.")
         elif self.recording:
             self.recording = False
+            self.status_bar.reset("Recording")
             self.save_record(self.record)
             print("Recording terminated.")
