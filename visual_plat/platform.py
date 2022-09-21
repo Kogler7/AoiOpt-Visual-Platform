8587613a29eb9ca0d32bddfca9a8d57f0414d326
魏振杰 <Kogler_S@outlook.com>
2022-09-21 22:08:07 +0800

    22w05c-4
    随便写点init文件

--- visual_plat/platform.py
+++ visual_plat/platform.py
@@ -1,11 +1,21 @@
 from PySide6.QtWidgets import QApplication
 from visual_plat.canvas import VisualCanvas
 from visual_plat.global_proxy.async_proxy import AsyncProxy
+"""
+可视化服务平台
+提供一些常规的设置服务
+"""
 
 
 class VisualPlatform:
     @staticmethod
     def launch(async_task: callable = None):
+        """
+        启动可视化平台
+        可附带执行一个异步任务
+        请不要在重复调用此函数
+        也不要在此函数后再执行其他任务
+        """
         app = QApplication([])
         canvas = VisualCanvas()
         if async_task is not None:
